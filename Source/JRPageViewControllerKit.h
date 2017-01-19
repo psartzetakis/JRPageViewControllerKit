94067ccadf6350eb97c4656fe427294e26849342
Panagiotis Sartzetakis <panos@sartzetakis.me>
2017-01-19 10:39:21 +0000

    tidy up

---
+++ Source/JRPageViewControllerKit.h
@@ -0,0 +1,15 @@
+//
+//  PageViewControllerKit.h
+//  PageViewControllerKit
+//
+//  Created by Panagiotis Sartzetakis on 28/05/2016.
+//  Copyright © 2016 Panagiotis Sartzetakis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double JRPageViewControllerKitVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char JRPageViewControllerKitVersionString[];
+
+
